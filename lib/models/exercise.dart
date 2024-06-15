46f6605aadcd15d1ac5e8237e9ead439becd3b93
Sevastian Rakhimov <Rakhimov.SE@gmail.com>
2024-06-15 04:28:52 +0300

    Update exercise.dart to work with new structure

--- lib/models/exercise.dart
+++ lib/models/exercise.dart
@@ -7,21 +7,30 @@ class Exercise {
   @JsonKey(name: 'ID')
   final int id;
   final String name;
-  final String muscle;
-  final String additionalMuscle;
-  final String type;
-  final String equipment;
-  final String difficulty;
+  @JsonKey(name: 'type_id')
+  final int typeId;
+  final ExerciseType type;
+  @JsonKey(name: 'difficulty_id')
+  final int difficultyId;
+  final Difficulty difficulty;
+  @JsonKey(name: 'Muscles')
+  final List<Muscle> muscles;
+  @JsonKey(name: 'AdditionalMuscles')
+  final List<Muscle> additionalMuscles;
+  @JsonKey(name: 'Equipments')
+  final List<Equipment> equipments;
   final List<ExercisePhoto> photos;
 
   Exercise({
     required this.id,
     required this.name,
-    required this.muscle,
-    required this.additionalMuscle,
+    required this.typeId,
     required this.type,
-    required this.equipment,
+    required this.difficultyId,
     required this.difficulty,
+    required this.muscles,
+    required this.additionalMuscles,
+    required this.equipments,
     required this.photos,
   });
 
@@ -31,9 +40,73 @@ class Exercise {
 }
 
 @JsonSerializable()
+class ExerciseType {
+  @JsonKey(name: 'ID')
+  final int id;
+  final String name;
+
+  ExerciseType({
+    required this.id,
+    required this.name,
+  });
+
+  factory ExerciseType.fromJson(Map<String, dynamic> json) =>
+      _$ExerciseTypeFromJson(json);
+  Map<String, dynamic> toJson() => _$ExerciseTypeToJson(this);
+}
+
+@JsonSerializable()
+class Difficulty {
+  @JsonKey(name: 'ID')
+  final int id;
+  final String level;
+
+  Difficulty({
+    required this.id,
+    required this.level,
+  });
+
+  factory Difficulty.fromJson(Map<String, dynamic> json) =>
+      _$DifficultyFromJson(json);
+  Map<String, dynamic> toJson() => _$DifficultyToJson(this);
+}
+
+@JsonSerializable()
+class Muscle {
+  @JsonKey(name: 'ID')
+  final int id;
+  final String name;
+
+  Muscle({
+    required this.id,
+    required this.name,
+  });
+
+  factory Muscle.fromJson(Map<String, dynamic> json) => _$MuscleFromJson(json);
+  Map<String, dynamic> toJson() => _$MuscleToJson(this);
+}
+
+@JsonSerializable()
+class Equipment {
+  @JsonKey(name: 'ID')
+  final int id;
+  final String name;
+
+  Equipment({
+    required this.id,
+    required this.name,
+  });
+
+  factory Equipment.fromJson(Map<String, dynamic> json) =>
+      _$EquipmentFromJson(json);
+  Map<String, dynamic> toJson() => _$EquipmentToJson(this);
+}
+
+@JsonSerializable()
 class ExercisePhoto {
   @JsonKey(name: 'ID')
   final int id;
+  @JsonKey(name: 'photoUrl')
   final String photoUrl;
 
   ExercisePhoto({
--- lib/models/exercise.g.dart
+++ lib/models/exercise.g.dart
@@ -9,11 +9,20 @@ part of 'exercise.dart';
 Exercise _$ExerciseFromJson(Map<String, dynamic> json) => Exercise(
       id: (json['ID'] as num).toInt(),
       name: json['name'] as String,
-      muscle: json['muscle'] as String,
-      additionalMuscle: json['additionalMuscle'] as String,
-      type: json['type'] as String,
-      equipment: json['equipment'] as String,
-      difficulty: json['difficulty'] as String,
+      typeId: (json['type_id'] as num).toInt(),
+      type: ExerciseType.fromJson(json['type'] as Map<String, dynamic>),
+      difficultyId: (json['difficulty_id'] as num).toInt(),
+      difficulty:
+          Difficulty.fromJson(json['difficulty'] as Map<String, dynamic>),
+      muscles: (json['Muscles'] as List<dynamic>)
+          .map((e) => Muscle.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      additionalMuscles: (json['AdditionalMuscles'] as List<dynamic>)
+          .map((e) => Muscle.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      equipments: (json['Equipments'] as List<dynamic>)
+          .map((e) => Equipment.fromJson(e as Map<String, dynamic>))
+          .toList(),
       photos: (json['photos'] as List<dynamic>)
           .map((e) => ExercisePhoto.fromJson(e as Map<String, dynamic>))
           .toList(),
@@ -22,12 +31,57 @@ Exercise _$ExerciseFromJson(Map<String, dynamic> json) => Exercise(
 Map<String, dynamic> _$ExerciseToJson(Exercise instance) => <String, dynamic>{
       'ID': instance.id,
       'name': instance.name,
-      'muscle': instance.muscle,
-      'additionalMuscle': instance.additionalMuscle,
-      'type': instance.type,
-      'equipment': instance.equipment,
-      'difficulty': instance.difficulty,
+      'type_id': instance.typeId,
+      'type': instance.type.toJson(),
+      'difficulty_id': instance.difficultyId,
+      'difficulty': instance.difficulty.toJson(),
+      'Muscles': instance.muscles.map((e) => e.toJson()).toList(),
+      'AdditionalMuscles':
+          instance.additionalMuscles.map((e) => e.toJson()).toList(),
+      'Equipments': instance.equipments.map((e) => e.toJson()).toList(),
       'photos': instance.photos.map((e) => e.toJson()).toList(),
+    };
+
+ExerciseType _$ExerciseTypeFromJson(Map<String, dynamic> json) => ExerciseType(
+      id: (json['ID'] as num).toInt(),
+      name: json['name'] as String,
+    );
+
+Map<String, dynamic> _$ExerciseTypeToJson(ExerciseType instance) =>
+    <String, dynamic>{
+      'ID': instance.id,
+      'name': instance.name,
+    };
+
+Difficulty _$DifficultyFromJson(Map<String, dynamic> json) => Difficulty(
+      id: (json['ID'] as num).toInt(),
+      level: json['level'] as String,
+    );
+
+Map<String, dynamic> _$DifficultyToJson(Difficulty instance) =>
+    <String, dynamic>{
+      'ID': instance.id,
+      'level': instance.level,
+    };
+
+Muscle _$MuscleFromJson(Map<String, dynamic> json) => Muscle(
+      id: (json['ID'] as num).toInt(),
+      name: json['name'] as String,
+    );
+
+Map<String, dynamic> _$MuscleToJson(Muscle instance) => <String, dynamic>{
+      'ID': instance.id,
+      'name': instance.name,
+    };
+
+Equipment _$EquipmentFromJson(Map<String, dynamic> json) => Equipment(
+      id: (json['ID'] as num).toInt(),
+      name: json['name'] as String,
+    );
+
+Map<String, dynamic> _$EquipmentToJson(Equipment instance) => <String, dynamic>{
+      'ID': instance.id,
+      'name': instance.name,
     };
 
 ExercisePhoto _$ExercisePhotoFromJson(Map<String, dynamic> json) =>
