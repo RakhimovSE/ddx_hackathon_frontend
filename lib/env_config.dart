17307651ae6d7b329d34cfc0c7a097d2d60ce2d7
Sevastian Rakhimov <Rakhimov.SE@gmail.com>
2024-06-15 21:34:47 +0300

    refactor: Update .env files and EnvConfig to support debug and release environments

--- lib/env_config.dart
+++ lib/env_config.dart
@@ -3,12 +3,10 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class EnvConfig {
   static Future<void> load() async {
-    await dotenv.load(fileName: ".env");
-
     if (kReleaseMode) {
-      await dotenv.load(fileName: ".env.release", mergeWith: dotenv.env);
+      await dotenv.load(fileName: ".env.release");
     } else {
-      await dotenv.load(fileName: ".env.debug", mergeWith: dotenv.env);
+      await dotenv.load(fileName: ".env.debug");
     }
   }
 }
