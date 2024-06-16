34ba6ccc696a0cdf98ec57571b621bea7cf4847d
Sevastian Rakhimov <Rakhimov.SE@gmail.com>
2024-06-16 09:34:02 +0300

    chore: Update intl dependency to version 0.19.0

--- lib/screens/main/training_schedule.dart
+++ lib/screens/main/training_schedule.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
 import '../../data/models/client_workout.dart';
 import 'training_schedule_card.dart';
 
@@ -11,9 +12,14 @@ class TrainingSchedule extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: workouts.map((workout) {
+        String time = 'No time specified';
+        if (workout.startDate != null) {
+          final DateFormat formatter = DateFormat.Hm();
+          time = formatter.format(workout.startDate!);
+        }
         return TrainingScheduleCard(
           title: workout.name,
-          time: workout.startDate?.toIso8601String() ?? 'No time specified',
+          time: time,
           trainer: 'Тренер', // Adjust as necessary
           imageUrl: 'https://example.com/training.jpg', // Placeholder
         );
