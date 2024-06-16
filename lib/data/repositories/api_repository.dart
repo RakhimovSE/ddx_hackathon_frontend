769cbb487d4ca57108e34cc089845853ec4d0d84
Sevastian Rakhimov <Rakhimov.SE@gmail.com>
2024-06-16 15:46:43 +0300

    feat: Add fetchClientWorkoutsByPlanId method to ApiRepository

--- lib/data/repositories/api_repository.dart
+++ lib/data/repositories/api_repository.dart
@@ -104,4 +104,19 @@ class ApiRepository {
       throw Exception('Failed to load client workouts');
     }
   }
+
+  Future<List<ClientWorkout>> fetchClientWorkoutsByPlanId(
+      int trainingPlanId) async {
+    final response = await http
+        .get(Uri.parse('$baseUrl/training_plans/$trainingPlanId/workouts'));
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse
+          .map((workout) => ClientWorkout.fromJson(workout))
+          .toList();
+    } else {
+      throw Exception('Failed to load workouts');
+    }
+  }
 }
