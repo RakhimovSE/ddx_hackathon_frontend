bcbd6a8f6e68bc31c7d9fe889453ae8b81a38233
Sevastian Rakhimov <Rakhimov.SE@gmail.com>
2024-06-23 18:42:59 +0500

    Fix TableCalendar for Android

--- lib/screens/main/calendar.dart
+++ lib/screens/main/calendar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class Calendar extends StatelessWidget {
@@ -17,31 +18,33 @@ class Calendar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: TableCalendar(
-        firstDay: DateTime.utc(2020, 1, 1),
-        lastDay: DateTime.utc(2030, 12, 31),
-        focusedDay: focusedDay,
-        selectedDayPredicate: (day) => isSameDay(selectedDay, day),
-        calendarFormat: CalendarFormat.week,
-        calendarStyle: const CalendarStyle(
-          selectedDecoration: BoxDecoration(
-            color: CupertinoColors.systemBlue,
-            shape: BoxShape.circle,
+      child: Material(
+        child: TableCalendar(
+          firstDay: DateTime.utc(2020, 1, 1),
+          lastDay: DateTime.utc(2030, 12, 31),
+          focusedDay: focusedDay,
+          selectedDayPredicate: (day) => isSameDay(selectedDay, day),
+          calendarFormat: CalendarFormat.week,
+          calendarStyle: const CalendarStyle(
+            selectedDecoration: BoxDecoration(
+              color: CupertinoColors.systemBlue,
+              shape: BoxShape.circle,
+            ),
+            todayDecoration: BoxDecoration(
+              color: CupertinoColors.lightBackgroundGray,
+              shape: BoxShape.circle,
+            ),
           ),
-          todayDecoration: BoxDecoration(
-            color: CupertinoColors.lightBackgroundGray,
-            shape: BoxShape.circle,
+          headerStyle: const HeaderStyle(
+            formatButtonVisible: false,
+            titleCentered: true,
+            titleTextStyle: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
           ),
+          onDaySelected: onDaySelected,
         ),
-        headerStyle: const HeaderStyle(
-          formatButtonVisible: false,
-          titleCentered: true,
-          titleTextStyle: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        onDaySelected: onDaySelected,
       ),
     );
   }
