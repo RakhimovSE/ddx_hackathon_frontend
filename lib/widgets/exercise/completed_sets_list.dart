112a734aba6febaedd56cba50dc4fdb19f1442dc
Sevastian Rakhimov <Rakhimov.SE@gmail.com>
2024-06-23 23:26:42 +0500

    refactor: Refactor CompletedSetsList widget for improved readability and maintainability

--- lib/widgets/exercise/completed_sets_list.dart
+++ lib/widgets/exercise/completed_sets_list.dart
@@ -1,73 +1,85 @@
 import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
+import '../../data/models/client_exercise_set.dart';
 import '../../data/models/client_workout_exercise.dart';
 
 class CompletedSetsList extends StatelessWidget {
   final List<ClientWorkoutExercise> completedExercises;
 
-  const CompletedSetsList({super.key, required this.completedExercises});
+  const CompletedSetsList({
+    Key? key,
+    required this.completedExercises,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: completedExercises
-          .map((exercise) => Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: Container(
-                  padding: const EdgeInsets.all(12.0),
-                  decoration: BoxDecoration(
-                    color: CupertinoColors.systemBackground,
-                    borderRadius: BorderRadius.circular(8.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: CupertinoColors.systemGrey.withOpacity(0.5),
-                        blurRadius: 5,
-                        offset: const Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        exercise.exercise.name,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 4),
-                      Text(
-                        exerciseSetsDescription(exercise),
-                        style: const TextStyle(
-                          fontSize: 14,
-                          color: CupertinoColors.systemGrey,
-                        ),
-                      ),
-                      const SizedBox(height: 4),
-                      Text(
-                        '${DateFormat('dd MMMM', 'ru').format(exercise.startDate ?? DateTime.now())} ${DateFormat('HH:mm').format(exercise.startDate ?? DateTime.now())}',
-                        style: const TextStyle(
-                          fontSize: 14,
-                          color: CupertinoColors.systemGrey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ))
-          .toList(),
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: completedExercises.map((exercise) {
+        return _buildExerciseSection(exercise);
+      }).toList(),
     );
   }
 
-  String exerciseSetsDescription(ClientWorkoutExercise exercise) {
-    return exercise.sets.map((set) {
-      if (set.duration > 0) {
-        return '${set.duration} сек';
-      } else {
-        return '${set.reps} раз';
-      }
-    }).join(', ');
+  Widget _buildExerciseSection(ClientWorkoutExercise exercise) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: Container(
+        padding: const EdgeInsets.all(12.0),
+        decoration: BoxDecoration(
+          color: CupertinoColors.systemBackground,
+          borderRadius: BorderRadius.circular(8.0),
+          boxShadow: [
+            BoxShadow(
+              color: CupertinoColors.systemGrey.withOpacity(0.5),
+              blurRadius: 5,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (exercise.startDate != null)
+              Text(
+                DateFormat('dd.MM').format(exercise.startDate!),
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: CupertinoColors.systemPurple,
+                ),
+              ),
+            const SizedBox(height: 8),
+            ...exercise.sets.map((set) => _buildSetRow(set)).toList(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSetRow(ClientExerciseSet set) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            '#${set.order + 1} ${set.restTime} кг x ${set.reps} повторов',
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          if (set.startDate != null)
+            Text(
+              DateFormat('HH:mm').format(set.startDate!),
+              style: const TextStyle(
+                fontSize: 16,
+                color: CupertinoColors.systemGrey,
+              ),
+            ),
+        ],
+      ),
+    );
   }
 }
