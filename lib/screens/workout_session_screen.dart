0d1b0bd7ff389b4d02e07a71858a2a35fe9e26f3
Sevastian Rakhimov <Rakhimov.SE@gmail.com>
2024-06-23 22:23:36 +0500

    feat: Add carousel_slider dependency for image carousel functionality
    This commit adds the carousel_slider dependency to the pubspec.yaml file, with version 4.2.1. The carousel_slider package will be used to implement an image carousel functionality in the app. This will allow users to swipe through a collection of images, enhancing the visual experience and providing a more interactive interface.
    
    Recent user commits:
    - Add navigation to WorkoutSessionScreen from WorkoutExercisesScreen
    - Update WorkoutExercisesScreen to display exercise equipment and sets description
    - Update ClientWorkoutExercise model to use exerciseId instead of workoutExerciseId
    - Fix TableCalendar for Android
    - Add lazy loading for images in MainScreen
    - Add internet permission to AndroidManifest.xml
    - Update StoriesList to use ClientTrainerBloc for fetching client trainers
    - Create README.md
    - Update ChatListScreen and ChatScreen for chat functionality
    - Add lazy loading for exercise images in WorkoutExercisesScreen

--- lib/screens/workout_session_screen.dart
+++ lib/screens/workout_session_screen.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import '../data/models/client_workout_exercise.dart';
 
 class WorkoutSessionScreen extends StatefulWidget {
   final int clientWorkoutId;
-  final List<ClientWorkoutExercise> exercises; // Принимаем список упражнений
+  final List<ClientWorkoutExercise> exercises;
 
-  const WorkoutSessionScreen(
-      {super.key, required this.clientWorkoutId, required this.exercises});
+  const WorkoutSessionScreen({
+    super.key,
+    required this.clientWorkoutId,
+    required this.exercises,
+  });
 
   @override
   _WorkoutSessionScreenState createState() => _WorkoutSessionScreenState();
@@ -15,154 +20,157 @@ class WorkoutSessionScreen extends StatefulWidget {
 
 class _WorkoutSessionScreenState extends State<WorkoutSessionScreen> {
   late List<ClientWorkoutExercise> exercises;
-  bool isLoading = true;
+  late int currentExerciseIndex;
+  late int currentSetIndex;
 
   @override
   void initState() {
     super.initState();
-    exercises = widget.exercises; // Используем переданный список упражнений
-    isLoading = false; // Так как упражнения уже переданы, загрузка не требуется
+    exercises = widget.exercises;
+    currentExerciseIndex = 0;
+    currentSetIndex = 0;
   }
 
   @override
   Widget build(BuildContext context) {
+    final currentExercise = exercises[currentExerciseIndex];
+    final currentSet = currentExercise.sets[currentSetIndex];
+
     return CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(
-        middle: Text('1/7'),
+      navigationBar: CupertinoNavigationBar(
+        middle: Text(
+            '${currentExerciseIndex + 1}/${exercises.length} упражнений выполнено'),
         previousPageTitle: 'Назад',
       ),
       child: SafeArea(
-        child: isLoading
-            ? const Center(child: CupertinoActivityIndicator())
-            : exercises.isEmpty
-                ? const Center(child: Text('Упражнений нет'))
-                : Column(
+        child: Column(
+          children: [
+            _buildImageCarousel(currentExercise),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    currentExercise.exercise.name,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    'Рекомендуемый вес: ${currentSet.restTime} кг',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: CupertinoColors.systemGrey,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      Image.network(
-                        'https://example.com/exercise.jpg', // Placeholder for exercise image
-                        width: double.infinity,
-                        height: 200,
-                        fit: BoxFit.cover,
-                        errorBuilder: (BuildContext context, Object exception,
-                            StackTrace? stackTrace) {
-                          return const Icon(CupertinoIcons.photo);
-                        },
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              "Упражнение",
-                              // exercises[0].workoutExercise.exercise.name,
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                            const Text(
-                              'Рекомендуемый вес: 4кг каждая гантеля',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: CupertinoColors.systemGrey,
-                              ),
-                            ),
-                            const SizedBox(height: 16),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                _buildInfoColumn('Повторений\nнужно', '12'),
-                                _buildInfoColumn('Время\nотдыха', '01:00'),
-                                _buildInfoColumn('Подходов\nсделано', '4.3'),
-                              ],
-                            ),
-                            const SizedBox(height: 16),
-                            const Text(
-                              'Килограмм',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                            const CupertinoTextField(
-                              placeholder: 'Килограмм',
-                            ),
-                            const SizedBox(height: 16),
-                            const Text(
-                              'Повторений',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                            const CupertinoTextField(
-                              placeholder: 'Повторений',
-                            ),
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        child: ListView.builder(
-                          itemCount: exercises.length,
-                          itemBuilder: (context, index) {
-                            final exercise = exercises[index];
-                            return Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 16.0, vertical: 8.0),
-                              child: Container(
-                                padding: const EdgeInsets.all(12.0),
-                                decoration: BoxDecoration(
-                                  color: CupertinoColors.systemBackground,
-                                  borderRadius: BorderRadius.circular(8.0),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: CupertinoColors.systemGrey
-                                          .withOpacity(0.5),
-                                      blurRadius: 5,
-                                      offset: const Offset(0, 3),
-                                    ),
-                                  ],
-                                ),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      // '#${index + 1} ${exercise.workoutExercise.exercise.name}',
-                                      '#${index + 1} Упражнение',
-                                      style: const TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    const SizedBox(height: 4),
-                                    Text(
-                                      exerciseSetsDescription(exercise),
-                                      style: const TextStyle(
-                                        fontSize: 14,
-                                        color: CupertinoColors.systemGrey,
-                                      ),
-                                    ),
-                                    const SizedBox(height: 4),
-                                    Text(
-                                      '${DateFormat('dd MMMM', 'ru').format(exercise.plannedStartDate ?? DateTime.now())} ${DateFormat('HH:mm').format(exercise.plannedStartDate ?? DateTime.now())}',
-                                      style: const TextStyle(
-                                        fontSize: 14,
-                                        color: CupertinoColors.systemGrey,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                      ),
+                      _buildInfoColumn(
+                          'Повторений\nнужно', '${currentSet.reps}'),
+                      _buildInfoColumn(
+                          'Время\nотдыха', '${currentExercise.restTime} сек'),
+                      _buildInfoColumn('Подходов\nсделано',
+                          '${currentSetIndex + 1}/${currentExercise.sets.length}'),
                     ],
                   ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'Килограмм',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  CupertinoTextField(
+                    placeholder: 'Килограмм',
+                    onChanged: (value) {
+                      // обработка изменений веса
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'Повторений',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  CupertinoTextField(
+                    placeholder: 'Повторений',
+                    onChanged: (value) {
+                      // обработка изменений повторений
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: exercises.length,
+                itemBuilder: (context, index) {
+                  final exercise = exercises[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 8.0),
+                    child: Container(
+                      padding: const EdgeInsets.all(12.0),
+                      decoration: BoxDecoration(
+                        color: CupertinoColors.systemBackground,
+                        borderRadius: BorderRadius.circular(8.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: CupertinoColors.systemGrey.withOpacity(0.5),
+                            blurRadius: 5,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '#${index + 1} ${exercise.exercise.name}',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            exerciseSetsDescription(exercise),
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: CupertinoColors.systemGrey,
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            '${DateFormat('dd MMMM', 'ru').format(exercise.plannedStartDate ?? DateTime.now())} ${DateFormat('HH:mm').format(exercise.plannedStartDate ?? DateTime.now())}',
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: CupertinoColors.systemGrey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            CupertinoButton.filled(
+              onPressed: _nextSet,
+              child: Text('Следующий подход'),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -188,6 +196,43 @@ class _WorkoutSessionScreenState extends State<WorkoutSessionScreen> {
         ),
       ],
     );
+  }
+
+  Widget _buildImageCarousel(ClientWorkoutExercise exercise) {
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: 200.0,
+        enlargeCenterPage: true,
+        enableInfiniteScroll: false,
+        initialPage: 0,
+      ),
+      items: exercise.exercise.photos.map((photo) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Image.network(
+              '${dotenv.env['API_URL']}/static/${photo.photoUrl}',
+              fit: BoxFit.cover,
+              width: 1000,
+              errorBuilder: (BuildContext context, Object exception,
+                  StackTrace? stackTrace) {
+                return const Icon(CupertinoIcons.photo);
+              },
+            );
+          },
+        );
+      }).toList(),
+    );
+  }
+
+  void _nextSet() {
+    setState(() {
+      if (currentSetIndex < exercises[currentExerciseIndex].sets.length - 1) {
+        currentSetIndex++;
+      } else if (currentExerciseIndex < exercises.length - 1) {
+        currentExerciseIndex++;
+        currentSetIndex = 0;
+      }
+    });
   }
 
   String exerciseSetsDescription(ClientWorkoutExercise exercise) {
