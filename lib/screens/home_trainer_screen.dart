f03fb56c3e224456d109121d98bbb0e54036a220
Sevastian Rakhimov <Rakhimov.SE@gmail.com>
2024-06-24 08:04:57 +0500

    Create home_trainer_screen.dart

---
+++ lib/screens/home_trainer_screen.dart
@@ -0,0 +1,62 @@
+import 'package:ddx_hackathon_frontend/screens/exercise_list_screen.dart';
+import 'package:flutter/cupertino.dart';
+import '../data/models/user.dart';
+import 'main/main_screen.dart';
+import 'profile_screen.dart';
+import 'trainers/trainer_screen.dart';
+
+class HomeTrainerScreen extends StatelessWidget {
+  final User userData;
+
+  const HomeTrainerScreen({super.key, required this.userData});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'Главная',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.person_2),
+            label: 'Тренеры',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.ellipses_bubble),
+            label: 'Упражнения',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.settings),
+            label: 'Настройки',
+          ),
+        ],
+      ),
+      tabBuilder: (context, index) {
+        switch (index) {
+          case 0:
+            return CupertinoTabView(
+              builder: (context) => const MainScreen(),
+            );
+          case 1:
+            return CupertinoTabView(
+              builder: (context) => const TrainerScreen(),
+            );
+          case 2:
+            return CupertinoTabView(
+              builder: (context) => const ExerciseListScreen(),
+            );
+          case 3:
+            return CupertinoTabView(
+              builder: (context) => ProfileScreen(userData: userData),
+            );
+          default:
+            return CupertinoTabView(
+              builder: (context) => const MainScreen(),
+            );
+        }
+      },
+    );
+  }
+}
